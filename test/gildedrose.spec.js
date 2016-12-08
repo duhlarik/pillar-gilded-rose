fd988a7b5f0e6d5f230356c74310e7a4fbeb1b60
Stephen Cavaliere <stephencavaliere@gmail.com>
2016-12-08 13:45:35 -0500

    Refactored tests

--- test/gildedrose.spec.js
+++ test/gildedrose.spec.js
@@ -1,131 +1,181 @@
 const expect = require('chai').expect;
-const Item = require('../src/gildedrose').Item;
+const Item = require('../src/item');
 const items = require('../src/gildedrose').items;
 const updateQuality = require('../src/gildedrose').update_quality;
 
-describe('Gilded Rose Inn', () => {
-  it('Basic item should decrease in quality at end of day', () => {
-    let item = new Item('random', 5, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(4);
-  });
-  it('Basic item should decrease in sell_in at end of day', () => {
-    let item = new Item('random', 5, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.sell_in).to.equal(4);
-  });
-  it('Two basic items should both decrease in quality at end of day', () => {
-    let item1 = new Item('random1', 5, 5);
-    let item2 = new Item('random2', 7, 7);
-    items.push(item1);
-    items.push(item2);
-    updateQuality();
-    expect(item1.quality).to.equal(4);
-    expect(item2.quality).to.equal(6);
-  });
-  it('Two basic items should both decrease in sell_in at end of day', () => {
-    let item1 = new Item('random1', 5, 5);
-    let item2 = new Item('random2', 7, 7);
-    items.push(item1);
-    items.push(item2);
-    updateQuality();
-    expect(item1.sell_in).to.equal(4);
-    expect(item2.sell_in).to.equal(6);
-  });
-  it('An item with negative sell_in decreases in quality by 2 at end of day', () => {
-    let item = new Item('random', -1, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(3);
-  });
-  it('An item with 0 sell_in decreases in quality by 2 at end of day', () => {
-    let item = new Item('random', 0, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(3);
-  });
-  it('An item with 1 sell_in decreases in quality by 1 at end of day', () => {
-    let item = new Item('random', 1, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(4);
-  });
-  it('An non-expired item with 1 quality should have 0 quality at end of day', () => {
-    let item = new Item('random', 1, 1);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(0);
-  });
-  it('An non-expired item with 0 quality should have 0 quality at end of day', () => {
-    let item = new Item('random', 1, 0);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(0);
-  });
-  it('An expired item with 1 quality should have 0 quality at end of day', () => {
-    let item = new Item('random', -1, 1);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(0);
-  });
-  it('An expired item with 0 quality should have 0 quality at end of day', () => {
-    let item = new Item('random', -1, 0);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(0);
-  });
-  it('An expired item with 2 quality should have 0 quality at end of day', () => {
-    let item = new Item('random', -1, 2);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(0);
-  });
-  it('Aged Brie should increase in quality by one at end of day', () => {
-    let item = new Item('Aged Brie', 5, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(6);
-  });
-  it('Aged Brie should continue to increase in quality by two after it has expired', () => {
-    let item = new Item('Aged Brie', -1, 5);
-    items.push(item);
-    updateQuality();
-    expect(item.quality).to.equal(7);
-  });
-
-  function shouldNotGoBeyond50(itemName) {
-    it(itemName + ' should not increase in quality beyond 50', () => {
-      let item = new Item(itemName, 1, 50);
-      items.push(item);
-      updateQuality();
-      expect(item.quality).to.equal(50);
-    });
+function removeAllItemsFromArray() {
+  while (items.length > 0) {
+    items.pop();
   }
+}
 
-  shouldNotGoBeyond50('Aged Brie');
-  shouldNotGoBeyond50('Backstage passes to a TAFKAL80ETC concert');
+describe('Gilded Rose Inn', () => {
+  beforeEach(removeAllItemsFromArray);
+  afterEach(removeAllItemsFromArray);
 
-  describe('Sulfuras, Hand of Ragnaros, is a legendary item:', () => {
-    it('Sulfuras shall always have a quality of 80', () => {
-      let item = new Item('Sulfuras, Hand of Ragnaros', 10, 80);
-      items.push(item);
-      updateQuality();
-      expect(item.quality).to.equal(80);
+  describe('Quality', () => {
+    describe('Basic Item', () => {
+      it('should decrease by one at the end of the day', () => {
+        const item = new Item('Basic Item', 5, 5);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(4);
+      });
+
+      it('should decrease by one for multiple items at the end of the day', () => {
+        const item1 = new Item('Basic Item 1', 5, 5);
+        const item2 = new Item('Basic Item 2', 7, 7);
+        items.push(item1);
+        items.push(item2);
+
+        updateQuality();
+
+        expect(item1.quality).to.equal(4);
+        expect(item2.quality).to.equal(6);
+      });
+
+      it('should never become negative', () => {
+        const item = new Item('Basic Item', 1, 0);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(0);
+      });
+
+      describe('Expired (sell_in turns negative)', () => {
+        it('should never become negative', () => {
+          const item = new Item('Basic Item', -1, 0);
+          items.push(item);
+
+          updateQuality();
+
+          expect(item.quality).to.equal(0);
+        });
+
+        it('should decrease by two', () => {
+          const item = new Item('Basic Item', 0, 5);
+          items.push(item);
+
+          updateQuality();
+
+          expect(item.quality).to.equal(3);
+        });
+      });
     });
-    it('Sulfuras shall always have a quality of 80 even if past sell by date', () => {
-      let item = new Item('Sulfuras, Hand of Ragnaros', -1, 80);
-      items.push(item);
-      updateQuality();
-      expect(item.quality).to.equal(80);
+
+    describe('Aged Brie', () => {
+      it('should increase by one at end of day', () => {
+        const item = new Item('Aged Brie', 5, 5);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(6);
+      });
+
+      it('should not become greater than 50', () => {
+        const item = new Item('Aged Brie', 1, 50);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(50);
+      });
+
+      describe('Expired (sell_in turns negative)', () => {
+        it('should increase by two at end of day', () => {
+          const item = new Item('Aged Brie', 0, 5);
+          items.push(item);
+
+          updateQuality();
+
+          expect(item.quality).to.equal(7);
+        });
+
+        it('should not become greater than 50', () => {
+          const item = new Item('Aged Brie', 1, 50);
+          items.push(item);
+
+          updateQuality();
+
+          expect(item.quality).to.equal(50);
+        });
+      });
     });
-    it('sell_in should not change', () => {
-      let item = new Item('Sulfuras, Hand of Ragnaros', 42, 80);
-      items.push(item);
-      updateQuality();
-      expect(item.sell_in).to.equal(42);
+
+    describe('Sulfuras, Hand of Ragnaros', () => {
+      it('should not decrease at end of day', () => {
+        const item = new Item('Sulfuras, Hand of Ragnaros', 10, 80);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(80);
+      });
+
+      describe('Expired (sell_in turns negative)', () => {
+        it('should not decrease at end of day', () => {
+          const item = new Item('Sulfuras, Hand of Ragnaros', -1, 80);
+          items.push(item);
+
+          updateQuality();
+
+          expect(item.quality).to.equal(80);
+        });
+      });
+    });
+
+    describe('Backstage passes to a TAFKAL80ETC concert', () => {
+      it('should not become greater than 50', () => {
+        const item = new Item('Backstage passes to a TAFKAL80ETC concert', 1, 50);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.quality).to.equal(50);
+      });
+
+      describe('Expired (sell_in turns negative)', () => {
+      });
     });
   });
 
+  describe('Sell In', () => {
+    describe('Basic Item', () => {
+      it('should decrease by one at the end of the day', () => {
+        const item = new Item('Basic Item', 5, 5);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.sell_in).to.equal(4);
+      });
+
+      it('should decrease by one for multiple items at the end of the day', () => {
+        const item1 = new Item('Basic Item 1', 5, 5);
+        const item2 = new Item('Basic Item 2', 7, 7);
+        items.push(item1);
+        items.push(item2);
+
+        updateQuality();
+
+        expect(item1.sell_in).to.equal(4);
+        expect(item2.sell_in).to.equal(6);
+      });
+    });
+
+    describe('Sulfuras, Hand of Ragnaros', () => {
+      it('should not decrease', () => {
+        const item = new Item('Sulfuras, Hand of Ragnaros', 42, 80);
+        items.push(item);
+
+        updateQuality();
+
+        expect(item.sell_in).to.equal(42);
+      });
+    });
+  });
 });
