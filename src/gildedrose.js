b3831ec29d29995caccb6854a9a5bc9e738fed1c
DeeAnn Uhlarik <duhlarik@pillartechnology.com>
2016-12-21 16:37:21 -0500

    rearranged if statements and removed unnessary logic

--- src/gildedrose.js
+++ src/gildedrose.js
@@ -3,23 +3,22 @@ var items = [];
 function update_quality() {
   items.forEach((item) => {
     if (item.name === 'Sulfuras, Hand of Ragnaros') { return; }
-    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (item.quality > 0) {
-        item.quality = updateItemQuality(item.quality, -1);
-      }
+    else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
     } else if (item.name === 'Aged Brie') {
       if (item.quality < 50) {
         item.quality = updateItemQuality(item.quality, 1);
       }
-    } else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-      item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
+    }
+    else if (item.quality > 0) {
+      item.quality = updateItemQuality(item.quality, -1);
     }
     item.sell_in = item.sell_in - 1;
     if (item.sell_in < 0) {
       if (item.name != 'Aged Brie') {
-          if (item.quality > 0) {
-            item.quality = updateItemQuality(item.quality, -1);
-          }
+        if (item.quality > 0) {
+          item.quality = updateItemQuality(item.quality, -1);
+        }
       } else {
         if (item.quality < 50) {
           item.quality = updateItemQuality(item.quality, 1);
