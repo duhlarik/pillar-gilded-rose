833103434a678a789794f52dd4c5ff6dffc2dc92
DeeAnn Uhlarik <duhlarik@pillartechnology.com>
2016-12-22 11:40:16 -0500

    pulled out a funtion for general items

--- src/gildedrose.js
+++ src/gildedrose.js
@@ -3,36 +3,29 @@ var items = [];
 function update_quality() {
   items.forEach((item) => {
     if (item.name === 'Sulfuras, Hand of Ragnaros') { return; }
-    else if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-      item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
-    } else if (item.name === 'Aged Brie') {
-      if (item.quality < 50) {
-        item.quality = updateItemQuality(item.quality, 1);
+    if (item.quality < 50) {
+      if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+        item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
+      } else if (item.name === 'Aged Brie') {
+        item.quality = updateAgedBrieQuality(item.quality, item.sell_in);
+      } else if (item.quality > 0) {
+        item.quality = updateItemQuality(item.quality, item.sell_in);
       }
-    }
-    else if (item.quality > 0) {
-      item.quality = updateItemQuality(item.quality, -1);
-    }
-    item.sell_in = item.sell_in - 1;
-    if (item.sell_in < 0) {
-      if (item.name != 'Aged Brie') {
-        if (item.quality > 0) {
-          item.quality = updateItemQuality(item.quality, -1);
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = updateItemQuality(item.quality, 1);
-        }
-      }
+      item.sell_in = item.sell_in - 1;
     }
   });
 }
 
-function updateItemQuality(itemQuality, qualityChange) {
-  if (itemQuality < 50) {
-    itemQuality = itemQuality + qualityChange;
-    return itemQuality;
-  }
+function updateItemQuality(itemQuality, sell_in) {
+  if (sell_in > 0) {
+    console.log('this is sell in > 0');
+    itemQuality -= 1;
+  } else
+    if (sell_in <= 0) {
+      console.log("this is sell in < 0");
+      itemQuality -= 2;
+    }
+  return itemQuality;
 }
 
 function updateBackstagePassQuality(backstagePassQuality, sell_in) {
@@ -62,6 +55,19 @@ function updateBackstagePassQuality(backstagePassQuality, sell_in) {
     backstagePassQuality = 0;
   }
   return backstagePassQuality;
+}
+
+function updateAgedBrieQuality(agedBrieQuality, sell_in) {
+  if (sell_in > 0) {
+    agedBrieQuality += 1;
+  } else {
+    if (agedBrieQuality <= 48) {
+      agedBrieQuality += 2;
+    } else {
+      agedBrieQuality += 1;
+    }
+  }
+  return agedBrieQuality;
 }
 
 module.exports.items = items;
