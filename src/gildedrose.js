d39273610ba833c68d7f471cea4e4eb3769e3f0d
Jennifer Kron <jkron@pillartechnology.com>
2016-12-22 14:12:53 -0500

    refactored quality logic into a switch case

--- src/gildedrose.js
+++ src/gildedrose.js
@@ -3,17 +3,25 @@ var items = [];
 function update_quality() {
   items.forEach((item) => {
     if (item.name === 'Sulfuras, Hand of Ragnaros') { return; }
-    if (item.quality < 50) {
-      if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
-        item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
-      } else if (item.name === 'Aged Brie') {
-        item.quality = updateAgedBrieQuality(item.quality, item.sell_in);
-      } else if (item.quality > 0) {
-        item.quality = updateItemQuality(item.quality, item.sell_in);
+    if (item.quality < 50 && item.quality > 0) {
+      switch (item.name) {
+        case 'Backstage passes to a TAFKAL80ETC concert':
+          item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
+          break;
+        case 'Aged Brie':
+          item.quality = updateAgedBrieQuality(item.quality, item.sell_in);
+          break;
+        default:
+          item.quality = updateItemQuality(item.quality, item.sell_in);
       }
-      item.sell_in = item.sell_in - 1;
-    }
-  });
+        item.sell_in = reduceSellInByOne(item.sell_in);
+      }
+    });
+}
+
+function reduceSellInByOne (sell_in) {
+  sell_in -= 1;
+  return sell_in;
 }
 
 function updateItemQuality(itemQuality, sell_in) {
@@ -29,24 +37,17 @@ function updateBackstagePassQuality(backstagePassQuality, sell_in) {
   if (sell_in > 10) {
     backstagePassQuality += 1;
   }
-  if (backstagePassQuality < 48) {
-    if (sell_in <= 5) {
-      backstagePassQuality += 3;
-    }
-    if (sell_in > 5 && sell_in <= 10) {
-      backstagePassQuality += 2;
-    }
+  if (sell_in <= 5) {
+    backstagePassQuality += 3;
   }
-  if (backstagePassQuality === 48) {
-    if (sell_in <= 10) {
-      backstagePassQuality += 2;
-    }
-  }
-  if (backstagePassQuality === 49) {
-    backstagePassQuality += 1;
+  if (sell_in > 5 && sell_in <= 10) {
+    backstagePassQuality += 2;
   }
   if (sell_in <= 0) {
     backstagePassQuality = 0;
+  }
+  if (backstagePassQuality > 50) {
+    backstagePassQuality = 50;
   }
   return backstagePassQuality;
 }
