e0fa540239cc93aa9202dca35384954085361170
DeeAnn Uhlarik <duhlarik@pillartechnology.com>
2016-12-29 15:58:52 -0500

    added updateItemQuality function to reduce complexity in update_quality function

--- src/gildedrose.js
+++ src/gildedrose.js
@@ -5,27 +5,18 @@ const MIN_ITEM_QUALITY = 0;
 function update_quality() {
   items.forEach((item) => {
     if (item.name === 'Sulfuras, Hand of Ragnaros') { return; }
-    if (itemQualityIsValid(item.quality, MAX_ITEM_QUALITY, MIN_ITEM_QUALITY)) {
-      switch (item.name) {
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
-          break;
-        case 'Aged Brie':
-          item.quality = updateItemThatGetsBetterWithAge(item.quality, item.sell_in, 1, 2);
-          break;
-        default:
-          item.quality = updateBasicItemQuality(item.quality, item.sell_in, -1, -2);
-      }
-      item.sell_in = reduceSellInByOne(item.sell_in);
+    switch (item.name) {
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        item.quality = updateBackstagePassQuality(item.quality, item.sell_in);
+        break;
+      case 'Aged Brie':
+        item.quality = updateItemThatGetsBetterWithAge(item.quality, item.sell_in, 1, 2);
+        break;
+      default:
+        item.quality = updateBasicItemQuality(item.quality, item.sell_in, -1, -2);
     }
+    item.sell_in = reduceSellInByOne(item.sell_in);
   });
-}
-
-function itemQualityIsValid(quality, MAX_ITEM_QUALITY, MIN_ITEM_QUALITY) {
-  if (quality < MAX_ITEM_QUALITY && quality > MIN_ITEM_QUALITY) {
-    var itemQualityValidity = true;
-    return itemQualityValidity;
-  }
 }
 
 function reduceSellInByOne(sell_in) {
@@ -35,8 +26,14 @@ function reduceSellInByOne(sell_in) {
 
 function updateItemQuality(itemQuality, amount) {
   itemQuality += amount;
-  if (itemQuality >= MAX_ITEM_QUALITY) { itemQuality = MAX_ITEM_QUALITY; }
-  else if (itemQuality <= MIN_ITEM_QUALITY) { itemQuality = MIN_ITEM_QUALITY; }
+
+  return validateItemQuality(itemQuality);
+}
+
+function validateItemQuality(itemQuality) {
+
+  itemQuality >= MAX_ITEM_QUALITY ? itemQuality = MAX_ITEM_QUALITY : itemQuality <= MIN_ITEM_QUALITY ? itemQuality = MIN_ITEM_QUALITY : itemQuality;
+
   return itemQuality;
 }
 
@@ -55,7 +52,7 @@ function updateBackstagePassQuality(backstagePassQuality, sell_in) {
     backstagePassQuality = updateItemQuality(backstagePassQuality, 1);
   }
   if (sell_in <= 5) {
-    backstagePassQuality = updateItemQuality(backstagePassQuality,2);
+    backstagePassQuality = updateItemQuality(backstagePassQuality, 2);
   }
   if (sell_in <= 0) {
     backstagePassQuality = 0;
@@ -65,9 +62,9 @@ function updateBackstagePassQuality(backstagePassQuality, sell_in) {
 
 function updateItemThatGetsBetterWithAge(itemQuality, sell_in, rateBeforeSell_in, rateAfterSell_in) {
   if (sell_in > 0) {
-    itemQuality = updateItemQuality (itemQuality, rateBeforeSell_in);
+    itemQuality = updateItemQuality(itemQuality, rateBeforeSell_in);
   } else {
-    itemQuality = updateItemQuality (itemQuality, rateAfterSell_in);
+    itemQuality = updateItemQuality(itemQuality, rateAfterSell_in);
   }
   return itemQuality;
 }
